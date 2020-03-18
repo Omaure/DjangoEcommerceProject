b0a01a8d99eded59cb8bbbd2eefcd6d927f205d3
Omaure <omarakram958@gmail.com>
2020-03-18 16:27:50 +0200

    Login MDB

--- onlineshopping/myshopping/urls.py
+++ onlineshopping/myshopping/urls.py
@@ -1,27 +1,27 @@
 from django.contrib import admin
 from django.urls import path
 from myshopping import views
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from . import views
-from .views import SearchResultsView,BootstrapFilterView
-from accounts.views import login_view,register_view,logout_view
+from .views import SearchResultsView, BootstrapFilterView
+from accounts.views import login_view, register_view, logout_view
 from cart.views import CartView, add_to_cart, remove_from_cart
 from myshopping.views import products
 from myshopping import urls
-urlpatterns=[
-    
+
+urlpatterns = [
+
     path('', views.Data_list, name='sublist'),
     path('products/', products, name='products'),
-    path('productbyname/<productName>/',views.displayProductData,name='productbyname'),
+    path('productbyname/<productName>/', views.displayProductData, name='productbyname'),
     # path('search/', SearchResultsView.as_view(), name='search_results'),
     path('search/', views.BootstrapFilterView, name='search_results'),
     path('accounts/login/', login_view),
-     path('accounts/signup/', register_view),
-     path('accounts/logout/', logout_view),
+    path('accounts/signup/', register_view),
+    path('accounts/logout/', logout_view),
     path('products/cart/<product_name>', add_to_cart, name='cart'),
     path('products/remove/<product_name>', remove_from_cart, name='remove-cart'),
     path('products/cartview/', CartView, name='cart-home'),
-    path('productbyname/<product_name>/cart/', add_to_cart, name='cart'),
-
+    path('productbyname/$/cart/<product_name>', add_to_cart, name='cart'),
 
 ]
--- onlineshopping/onlineshopping/urls.py
+++ onlineshopping/onlineshopping/urls.py
@@ -35,14 +35,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url,include
-from myshopping import views,urls
+from django.conf.urls import url, include
+from myshopping import views, urls
 from cart import urls
 from myshopping.views import displayProductData
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-   path('',include('myshopping.urls')),
+                  path('admin/', admin.site.urls),
+                  path('', include('myshopping.urls')),
 
-
-]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
-
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
